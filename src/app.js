0a6268a6a34e0c69f5ce411be8029564f7a498b4
Fekete Gergely <feketegergely062@gmail.com>
2023-03-10 10:45:26 +0100

    tömb

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const tbody = document.querySelector("#tbody");
+
+
+var dolgozoLista = [
+   
+    { name : "Pisti", city: "Szolnok", salary: 385 },
+    { name : "Mari", city: "Szeged", salary: 381 },
+    { name : "Kati", city: "Szolnok", salary: 383 },
+    { name : "Dani", city: "Szeged", salary: 380 },
+    { name : "Lili", city: "Szolnok", salary: 382 },
+
+];
+
+console.log(tbody)
